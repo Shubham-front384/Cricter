1c8405d9c5c9a1f7249734c70a12d661b032b948
Shubham-front384 <shubhamworkemail07@gmail.com>
2025-11-15 13:43:19 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,340 @@
+const iplTeams = [
+  {
+    teamName: "CSK",
+    primaryColor: "#F9CD05",
+    secondaryColor: "#1F2F57",
+    captain: "Ruturaj Gaikwad",
+    captainImg: "./Img/dhoniSir.png",
+    teamLogo: "./Img/csk.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2010 },
+      { img: "./Img/trophy.png", year: 2011 },
+      { img: "./Img/trophy.png", year: 2018 },
+      { img: "./Img/trophy.png", year: 2021 },
+      { img: "./Img/trophy.png", year: 2023 }
+    ],
+
+    teamMembers2025: [
+      "Ruturaj Gaikwad",
+      "MS Dhoni",
+      "Ravindra Jadeja",
+      "Deepak Chahar",
+      "Matheesha Pathirana",
+      "Daryl Mitchell",
+      "Shivam Dube",
+      "Tushar Deshpande",
+      "Prashant Solanki",
+      "Mukesh Choudhary"
+    ],
+  },
+
+  {
+    teamName: "MI",
+    primaryColor: "#004BA0",
+    secondaryColor: "#D1AB3E",
+    captain: "Hardik Pandya",
+    captainImg: "./Img/hardik.png",
+    teamLogo: "./Img/mi.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2013 },
+      { img: "./Img/trophy.png", year: 2015 },
+      { img: "./Img/trophy.png", year: 2017 },
+      { img: "./Img/trophy.png", year: 2019 },
+      { img: "./Img/trophy.png", year: 2020 }
+    ],
+
+    teamMembers2025: [
+      "Hardik Pandya",
+      "Rohit Sharma",
+      "Suryakumar Yadav",
+      "Ishan Kishan",
+      "Jasprit Bumrah",
+      "Tilak Varma",
+      "Tim David",
+      "Gerald Coetzee",
+      "Romario Shepherd",
+      "Shams Mulani"
+    ],
+  },
+
+  {
+    teamName: "RCB",
+    primaryColor: "#DA291C",
+    secondaryColor: "#000000",
+    captain: "Faf du Plessis",
+    captainImg: "./Img/faf.png",
+    teamLogo: "./Img/rcb.png",
+
+    trophies: [],
+
+    teamMembers2025: [
+      "Faf du Plessis",
+      "Virat Kohli",
+      "Glenn Maxwell",
+      "Mohammed Siraj",
+      "Cameron Green",
+      "Rajat Patidar",
+      "Dinesh Karthik",
+      "Will Jacks",
+      "Reece Topley",
+      "Vyshak Vijaykumar"
+    ],
+  },
+
+  {
+    teamName: "KKR",
+    primaryColor: "#3A225D",
+    secondaryColor: "#D4A92A",
+    captain: "Ajinkya Rahane",
+    captainImg: "./Img/Ajinkya.png",
+    teamLogo: "./Img/kkr.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2012 },
+      { img: "./Img/trophy.png", year: 2014 },
+      { img: "./Img/trophy.png", year: 2024 }
+    ],
+
+    teamMembers2025: [
+      "Ajinkya Rahane",
+      "Shreyas Iyer",
+      "Sunil Narine",
+      "Andre Russell",
+      "Rinku Singh",
+      "Mitchell Starc",
+      "Rahmanullah Gurbaz",
+      "Varun Chakravarthy",
+      "Harshit Rana",
+      "Jason Roy"
+    ],
+  },
+
+  {
+    teamName: "RR",
+    primaryColor: "#EA1A8E",
+    secondaryColor: "#004BA0",
+    captain: "Sanju Samson",
+    captainImg: "./Img/SANJU.png",
+    teamLogo: "./Img/rr.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2008 }
+    ],
+
+    teamMembers2025: [
+      "Sanju Samson",
+      "Yashasvi Jaiswal",
+      "Jos Buttler",
+      "Riyan Parag",
+      "Trent Boult",
+      "Shimron Hetmyer",
+      "Ravichandran Ashwin",
+      "Avesh Khan",
+      "Sandeep Sharma",
+      "Dhruv Jurel"
+    ],
+  },
+
+  {
+    teamName: "SH",
+    primaryColor: "#F26522",
+    secondaryColor: "#000000",
+    captain: "Pat Cummins",
+    captainImg: "./Img/Pat.png",
+    teamLogo: "./Img/srh.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2016 }
+    ],
+
+    teamMembers2025: [
+      "Pat Cummins",
+      "Heinrich Klaasen",
+      "Abhishek Sharma",
+      "Aiden Markram",
+      "Bhuvneshwar Kumar",
+      "Travis Head",
+      "T Natarajan",
+      "Mayank Agarwal",
+      "Marco Jansen",
+      "Washington Sundar"
+    ],
+  },
+
+  {
+    teamName: "DC",
+    primaryColor: "#004BA0",
+    secondaryColor: "#C62828",
+    captain: "Axar Patel",
+    captainImg: "./Img/axar.png",
+    teamLogo: "./Img/dc.png",
+
+    trophies: [],
+
+    teamMembers2025: [
+      "Axar Patel",
+      "Rishabh Pant",
+      "David Warner",
+      "Prithvi Shaw",
+      "Kuldeep Yadav",
+      "Ishant Sharma",
+      "Jake Fraser-McGurk",
+      "Khaleel Ahmed",
+      "Lalit Yadav",
+      "Anrich Nortje"
+    ],
+
+    iplName: "IPL Team - DC"
+  },
+
+  {
+    teamName: "PK",
+    primaryColor: "#ED1B24",
+    secondaryColor: "#D4AF37",
+    captain: "Shreyas Iyer",
+    captainImg: "./Img/Shreyas.png",
+    teamLogo: "./Img/pk.png",
+
+    trophies: [],
+
+    teamMembers2025: [
+      "Shreyas Iyer",
+      "Shikhar Dhawan",
+      "Liam Livingstone",
+      "Kagiso Rabada",
+      "Arshdeep Singh",
+      "Sam Curran",
+      "Shashank Singh",
+      "Harpreet Brar",
+      "Jonny Bairstow",
+      "Rahul Chahar"
+    ],
+  },
+
+  {
+    teamName: "GT",
+    primaryColor: "#1B2133",
+    secondaryColor: "#AFAFAF",
+    captain: "Shubman Gill",
+    captainImg: "./Img/shubham.png",
+    teamLogo: "./Img/gt.png",
+
+    trophies: [
+      { img: "./Img/trophy.png", year: 2022 }
+    ],
+
+    teamMembers2025: [
+      "Shubman Gill",
+      "Rashid Khan",
+      "Rahul Tewatia",
+      "David Miller",
+      "Mohit Sharma",
+      "Sai Sudharsan",
+      "Matthew Wade",
+      "Josh Little",
+      "Vijay Shankar",
+      "Wriddhiman Saha"
+    ],
+  },
+
+  {
+    teamName: "LSG",
+    primaryColor: "#1D84B5",
+    secondaryColor: "#F58220",
+    captain: "Rishabh Pant",
+    captainImg: "./Img/Rishabh.png",
+    teamLogo: "./Img/lsg.png",
+
+    trophies: [],
+
+    teamMembers2025: [
+      "Rishabh Pant",
+      "KL Rahul",
+      "Marcus Stoinis",
+      "Nicholas Pooran",
+      "Ravi Bishnoi",
+      "Quinton de Kock",
+      "Krunal Pandya",
+      "Naveen-ul-Haq",
+      "Deepak Hooda",
+      "Mayank Yadav"
+    ],
+  }
+];
+
+let button = document.querySelector(".ipl-btn button");
+let heading = document.querySelector("h1");
+let trophyWrapper = document.querySelector(".trophy-details");
+let section = document.querySelector("section");
+let iplTeam = document.querySelector(".ipl-team");
+let captainImg = document.querySelector(".captain-img");
+let captainLogo = document.querySelector(".captain-logo");
+
+function renderTeam(team) {
+  heading.style.color = team.secondaryColor;
+  button.style.backgroundColor = team.secondaryColor;
+  iplTeam.style.backgroundColor = team.secondaryColor;
+  heading.innerText = team.teamName;
+  
+  trophyWrapper.innerHTML = "";
+  iplTeam.innerHTML = "";
+
+  if (team.trophies.length > 1) {
+    trophyWrapper.style.gridTemplateColumns = "1fr 1fr";
+  } else {
+    trophyWrapper.style.gridTemplateColumns = "1fr";
+  }
+
+  section.style.backgroundColor = team.primaryColor;
+
+  team.trophies.forEach(val => {
+    let divCreate = document.createElement("div");
+    let imageCreate = document.createElement("img");
+    let h5Create = document.createElement("h5");
+
+    imageCreate.src = val.img;
+    h5Create.innerText = val.year;
+
+    divCreate.classList.add("ipl-trophy");
+    h5Create.classList.add("trophy-year");
+
+    divCreate.appendChild(imageCreate);
+    divCreate.appendChild(h5Create);
+
+    trophyWrapper.style.background = team.secondaryColor;
+    trophyWrapper.appendChild(divCreate);
+  });
+
+  team.teamMembers2025.forEach(val => {
+    let memberName = document.createElement("h5");
+    memberName.classList.add("ipl-name");
+
+    memberName.innerText = val;
+
+    iplTeam.appendChild(memberName);
+  });
+
+  let captainimg = document.createElement("img");
+  captainimg.setAttribute("src", team.captainImg);
+  captainimg.setAttribute("alt", "captain-img");
+
+  captainImg.innerHTML = "";
+  captainImg.appendChild(captainimg);
+
+  let captainlogo = document.createElement("img");
+  captainlogo.setAttribute("src", team.teamLogo);
+  captainlogo.setAttribute("alt", "teamLogo-img");
+  captainlogo.classList.add("logo");
+
+  captainLogo.innerHTML = "";
+  captainLogo.appendChild(captainlogo);
+}
+
+renderTeam(iplTeams[0]);
+
+button.addEventListener("click", () => {
+  let randomTeam = iplTeams[Math.floor(Math.random() * iplTeams.length)];
+  renderTeam(randomTeam);
+});
